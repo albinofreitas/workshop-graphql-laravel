37688207f9adfb12c851948a5e6ee8b75f3ec27c
Albino Freitas <freitas@mageda.com.br>
2019-12-18 02:02:48 -0300

    generate dummy data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,11 +6,26 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $users = factory(App\User::class, 10)
+            ->create()
+            ->each(function ($user) {
+                $user->posts()->createMany(
+                    factory(App\Post::class, 20)
+                        ->make()->toArray()
+                );
+            })
+        ;
+
+        foreach ($users as $user) {
+            foreach ($user->posts as $post) {
+                $post->comments()->createMany(
+                    factory(App\Comment::class, 10)
+                        ->make()->toArray()
+                );
+            }
+        }
     }
 }
